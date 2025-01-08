da7391c21bc7dce7576b0734b7c04d2fc8a5f482
optimus_mc <muadongyeuthuong3x>
2025-01-08 16:19:58 +0700

    update

--- src/services/sim.service.ts
+++ src/services/sim.service.ts
@@ -84,7 +84,7 @@ const SimService = {
     }
   },
   getInformation: async(req: Request, res: Response): Promise<void> => {
-    res.status(201).json({ message: "Success hook" });
+    res.status(201).json({ message: "Success hoo111111111111111k" });
   },  
   addVolumeInSims: async (data: any): Promise<void> => {
     const { sims, bank, token_nce } = data;
